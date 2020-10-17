11f4fca3aeb09f2d113f4aa4318770cf66ecafcf
KushSalaskar <kushsalaskar@gmail.com>
2020-10-17 23:20:27 +0530

    Updates

---
+++ precache-manifest.a1f07b93d93e7d1f858f77d4042bc369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dc7c4596e3b1026c783c2d229e7c090",
+    "url": "/Hangman/index.html"
+  },
+  {
+    "revision": "4ef4ea6d2c65e8908fe7",
+    "url": "/Hangman/static/css/main.d07be7c6.chunk.css"
+  },
+  {
+    "revision": "5de3f477c7a72f2f89be",
+    "url": "/Hangman/static/js/2.5dfd5a7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hangman/static/js/2.5dfd5a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ef4ea6d2c65e8908fe7",
+    "url": "/Hangman/static/js/main.7fadb800.chunk.js"
+  },
+  {
+    "revision": "03c3bec1ad2fe6d82002",
+    "url": "/Hangman/static/js/runtime-main.6c19cf00.js"
+  }
+]);
